9ac80b3eb76a5ce84244ad1c7bbc90a91b2c4361
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2026-04-21 02:27:28 +0100

    new update

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-  return  "Version 2 - Deployee automatiquement"
+  return  "Version 2 - Deployee manual"
 if __name__=="__main__":
  app.run(host="0.0.0.0",port=5000)
